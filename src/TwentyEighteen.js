02e70dec861e92a9cf802f351ba7208866c1ccf8
Mark <duffy_mark@hotmail.com>
2022-04-12 12:21:32 -0300

    img fixes

--- src/TwentyEighteen.js
+++ src/TwentyEighteen.js
@@ -6,7 +6,7 @@ const TwentyEighteen = () => {
              
             <h2>2018</h2>
             <div className="imageContainer">
-            <img src={hawaii} alt="image of hawaii"/>
+            <img src={hawaii} alt="Hawaiian Beach"/>
             <p>- while working in insurance my wife received an offer to teach at the University of Hawaii in Honolulu.</p>
             </div>
             <p class="pTwo">- left my career to move abroad and become a stay-at-home-father for our 2 year old daughter.</p>
